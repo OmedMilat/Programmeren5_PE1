c9514b978b2a4057b15df99e024f19c82b6b83c2
Milat Omed <qais187@gmail.com>
2017-10-04 15:43:59 +0200

    first commit

---
+++ B4.PE1.OmedMilat/B4.PE1.OmedMilat/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace B4.PE1.OmedMilat
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnPicture_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PicturePage());
+        }
+
+        private async void btnWiki_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new WikiPage());
+        }
+    }
+}
---
+++ B4.PE1.OmedMilat/B4.PE1.OmedMilat/PicturePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace B4.PE1.OmedMilat
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PicturePage : ContentPage
+    {
+        public PicturePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnBack_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync(true);
+        }
+    }
+}
